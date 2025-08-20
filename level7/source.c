5d1d68818d46691125e3a63f3f45bb25f6616c36
hfanzaou <40872984+hfanzaou@users.noreply.github.com>
2025-08-20 22:40:45 +0100

    Update source.c

--- level7/source.c
+++ level7/source.c
@@ -1,10 +1,5 @@
 char c[68]; // global buffer for fgets
 
-void n(void) {
-    // hidden function – reads and prints password
-    system("/bin/cat /home/user/level9/.pass");
-}
-
 void m(void *param1, int param2, char *param3, int param4, int param5) {
     time_t t;
     t = time(NULL);
@@ -16,17 +11,17 @@ int main(int argc, char **argv) {
     FILE *f;
 
     // allocate 2 chunks (struct-like)
-    a = malloc(8);
+    a = malloc(8); // Chunk A
     *a = (void *)1;
-    a[1] = malloc(8);
+    a[1] = malloc(8); // Chunk B
 
-    b = malloc(8);
+    b = malloc(8); // Chunk C
     *b = (void *)2;
-    b[1] = malloc(8);
+    b[1] = malloc(8); // Chunk D
 
     // vulnerable copies
-    strcpy((char *)a[1], argv[1]);
-    strcpy((char *)b[1], argv[2]);
+    strcpy((char *)a[1], argv[1]); // copy argv[1] into Chunk B
+    strcpy((char *)b[1], argv[2]); // copy argv[1] into Chunk D
 
     // load secret password into global buffer
     f = fopen("/home/user/level8/.pass", "r");
